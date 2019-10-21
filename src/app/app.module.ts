02989ca1ad56fd91e77bea734dfbbe659d6ede20
SBORCHIA <56228585+SBORCHIA@users.noreply.github.com>
2019-10-21 00:02:58 -0300

    Boletim UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { Md5 } from 'ts-md5';
+import { Md5 } from 'ts-md5'; 
+
+
 
 @NgModule({
   declarations: [AppComponent],
