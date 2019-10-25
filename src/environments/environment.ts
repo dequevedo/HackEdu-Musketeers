0d908dce35a6c4e4bb1986e7e50d01e387c88d12
SBORCHIA <56228585+SBORCHIA@users.noreply.github.com>
2019-10-25 12:29:35 -0300

    t

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCB8qzQKMirjrzE2og6ouLtQ1zal1dMkAc",
+    authDomain: "portalseile.firebaseapp.com",
+    databaseURL: "https://portalseile.firebaseio.com",
+    projectId: "portalseile",
+    storageBucket: "portalseile.appspot.com",
+    messagingSenderId: "377254006441",
+    appId: "1:377254006441:web:f26c07c2e806dfb324e3b8",
+    measurementId: "G-ETZ8DDMDZN"
+
+  }
 };
 
 /*
